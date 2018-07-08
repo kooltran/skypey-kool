8eecd5c99ab93b0c1e63630e626057b7472b7192
kooltran <kiettrankm11@gmail.com>
2018-07-08 23:28:35 +0700

    create redux exercises

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './store/store'
+
+function fancyLog() {
+  console.log("%c Rendered with 👉 👉👇", "background: purple; color: #FFF");
+  console.log(store.getState());
+}
+
+const render = () => {
+  // fancyLog();
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
+render();
+store.subscribe(render);
